0dd1b1602ee178fdd3e63b76739187d63a39a1bd
Andre <curi_asa@hotmail.com>
2021-04-23 20:39:52 -0300

    Atividade Estação Espacial
    Movimentação em Navmesh Unity

---
+++ Assets/AIControl.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIControl : MonoBehaviour
+{
+    // variavel de NavMeshAgent
+    public NavMeshAgent agent;
+    void Start()
+    {
+        //acessando component NavMeshAgent em agent
+        agent = this.GetComponent<NavMeshAgent>();
+    }
+}
---
+++ Assets/AgentManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentManager : MonoBehaviour
+{
+    //array de agents
+    GameObject[] agents;
+    void Start()
+    {
+        //fazendo identificação de agents por tag "ai"
+        agents = GameObject.FindGameObjectsWithTag("ai");
+    }
+
+
+    void Update()
+    {
+        //configurando botão 0 do mouse
+        if (Input.GetMouseButtonDown(0))
+        {
+            //gerando raycast
+            RaycastHit hit;
+
+            //configurando raycast para posição conforme click do mouse
+            if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+            {
+                foreach (GameObject a in agents)
+                    a.GetComponent<AIControl>().agent.SetDestination(hit.point);
+            }
+        }
+    }
+}
